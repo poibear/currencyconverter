042dab409bbed9ec113103ab2b64444f78365b52
poiboi <33038633+poibear@users.noreply.github.com>
2022-08-03 20:04:29 -0700

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,101 @@
+import sys
+
+sys.path.insert(1, "C:\\Users\\JoshL\\Python\\projects\\currencyconvert") #allow import of currencyconvert from folder
+
+import tkinter as tk
+from currencyconvert import convertCurrency
+
+class CurrencyConversion(tk.Frame):
+
+    bgprocess = convertCurrency(gui=True)
+
+    def __init__(self):
+        self.master = self._draw()
+        self.bgprocess = convertCurrency(gui=True)
+        
+    def convInEntryBox(self, finalStringVar, currentCurrency, currentBalance, finalCurrency):
+        current = currentCurrency.get()
+        toconvert = finalCurrency.get()
+        balance = currentBalance.get()
+        info = self.bgprocess.getCurrency(current, balance, toconvert)
+        finalStringVar.set(info[2])
+        return info[2] #not necessary but recommended to info what we're changing
+
+    def _draw(self):
+        print('using default config...')
+        # base format
+        mainframe = tk.Tk()
+        mainframe.title('currency converter')
+        mainframe.geometry('960x540')
+        
+        maincontent = tk.Frame(mainframe)
+
+        fromframe = tk.Frame(maincontent, background="#99b1ff")
+        toframe = tk.Frame(maincontent, background="#acff99")
+
+        # row/column config
+        for rc in range(1):
+            fromframe.grid_columnconfigure(rc, weight = 1)
+            toframe.grid_columnconfigure(rc, weight = 1)
+        
+        toframe.grid_rowconfigure((0, 1), weight = 1)
+        
+        currencylist = self.bgprocess.currenciesToList()
+
+        # elements
+        currentbalancetext = tk.StringVar(fromframe) # ORIGINAL CURRENCY TEXT
+        currentbalance = tk.Entry(fromframe, textvariable=currentbalancetext)
+        
+        tocurrencytext = tk.StringVar(toframe)
+        tocurrencybalance = tk.Entry(toframe, textvariable=tocurrencytext, state=tk.DISABLED)
+        
+        currentcurrency = tk.StringVar(fromframe)
+        currentcurrency.set(currencylist[currencylist.index('USD')]) #must add this function separate from var assignment to use .get function
+        currentcurrencydropdown = tk.OptionMenu(fromframe, currentcurrency, *currencylist)
+        
+        finalcurrency = tk.StringVar(toframe)
+        finalcurrency.set(currencylist[currencylist.index('USD')])
+        finalcurrencydropdown = tk.OptionMenu(toframe, finalcurrency, *currencylist)
+        
+        def conv():
+            current = currentcurrency.get()
+            toconvert = tocurrencytext.get()
+            balance = currentbalancetext.get()
+            info = self.bgprocess.getCurrency(current, balance, toconvert)
+            finalcurrency.set(info[2])
+            return info[2] #not necessary but recommended to info what we're changing
+        
+        convertbutton = tk.Button(toframe, text="convert", command=self.convInEntryBox(finalcurrency, ))
+        
+        # init elements
+        
+        currentbalance.grid(row=0, column=0, sticky="nsew")
+        currentcurrencydropdown.grid(row=0, column=1, sticky="nsew")
+        
+        tocurrencybalance.grid(row=0, column=0, sticky="nsew")
+        finalcurrencydropdown.grid(row=0, column=1, sticky="nsew")
+        
+        convertbutton.grid(row=1, columnspan=2, sticky="nsew")
+        
+        # init frames
+        maincontent.pack(fill="both", expand=True)
+        
+        fromframe.pack(side="top", fill="both", expand=True)
+        toframe.pack(side="bottom", fill="both", expand=True)
+        
+        print('done!')
+        return mainframe
+    
+    def run(self):
+        def completeKill():
+            self.master.quit()
+            self.master.destroy()
+            print('destroyed root window')
+        try:
+            self.master.wm_protocol('WM_DELETE_WINDOW', completeKill)
+            self.master.mainloop()
+        except AttributeError:
+            print('you haven\'t set a valid master tk frame for the class yet!\neither leave the parameters be or initialize your own with its custom setup!')
+
+obj1 = CurrencyConversion()
+obj1.run()
---
+++ currencyconvert.py
@@ -0,0 +1,106 @@
+import requests
+import time
+
+class convertCurrency:
+
+    currencyrates = "https://api.exchangerate.host/latest?"
+
+    def __init__(self, gui=False):
+        #if using a gui with this class, set gui to true
+        self.gui = gui
+    
+    def fetchCurrencies(self, *args):
+        """
+        Gets the current list of available currencies from http://exchangerate.host
+        Input: *args: str #represents the queries to the API
+        Output:
+            json: dict
+        """
+        currencyurl = convertCurrency().currencyrates
+        if args:
+            for value in args:
+                currencyurl += value
+        currencyfetch = requests.get(currencyurl)
+        return currencyfetch.json()
+    
+    def currenciesToList(self):
+        """
+        Fetches all the known currencies from the API as a JSON and converts it into a list
+        
+        Output:
+            list: list of all currencies that can be converted
+        """
+        currencyjson = self.fetchCurrencies()
+        currencylist = []
+        for k, v in currencyjson['rates'].items():
+            currencylist.append(k)
+        return currencylist
+    
+    def checkforCurrency(self, currencyjson, *args):
+        #TODO check whether any of the given currencies are valid, e.g. USD is a valid but AOR isn't
+        """
+        Checks whether the given currencies exist in the given json from the api
+        Input: currencyjson: dict, *args: str, currencyjson being the list of currencies from the api and args being the 3 initials of the desired currency
+        Output:
+            bool, Boolean represents whether the given currencies exist (returns False if any of the currencies dont exist)
+        """
+        args = tuple((arg.upper() for arg in args))
+        for key, value in currencyjson["rates"].items():
+            for currency in args:
+                if key == currency:
+                    print(f"found exchange rate for currency \"{key}\"")
+                    time.sleep(0.5)
+                    return True
+
+                elif key == list(currencyjson["rates"])[-1] and currency != key:
+                    print("couldn't find your currency on the list. exiting...")
+                    return False
+
+    def getCurrency(self, currentcurrency=None, currentbalance=None, newcurrency=None):
+        """
+        Fetches inputted currencies and returns the exchange rate of one currency for each given.
+        Input:
+            currentcurrency: str
+            currentbalance: str/float
+            newcurrency: str
+            *all are applicable when parameter "gui" is true, otherwise not necessary
+        Output:
+            Balance of Original Currency: float
+            Currency Name: str
+            Balance of New Currency: float
+            New Currency Name: str
+        Errors:
+            ValueError: Invalid Currency
+            SyntaxError: Conversion Error with Invalid Input
+        """
+        #TODO create different handle for incorrect input of currency, e.g., input of USA instead of USD (fuzzy search)
+        if self.gui is False:
+            if currentcurrency is None:
+                currentcurrency = input("what currency would you like to convert? (first three initials) ").upper() #doesnt matter whether it's uppercase or not, for convenience
+            if currentbalance is None:
+                convertto = input(f"what currency do you want your {currentcurrency} converted to? (first three initials) ").upper()
+            if newcurrency is None:
+                currentbalance = float(input(f"how much of your \"{currentcurrency}\" would you like to convert into \"{convertto}\"? (format: 000.00) "))
+            
+        currentcurrency = currentcurrency.upper() or currentcurrency
+        convertto = newcurrency.upper() or convertto
+        currencyjson = self.fetchCurrencies()
+        
+        if self.checkforCurrency(currencyjson, currentcurrency) == True:
+            currencyjson = self.fetchCurrencies(f"base={currentcurrency}")
+        else:
+            raise ValueError('Currency doesn\'t exist.')
+            
+        converted = float(currentbalance) * currencyjson["rates"][f"{convertto.upper()}"]
+        try:
+            if self.gui is False:
+                print(f"your ${currentbalance:.2f} in \"{currentcurrency}\" is ${converted:.2f} in \"{convertto}\".")
+        except ValueError:
+            print(f'Invalid input for parameter \'currentbalance\'\nReason: Type \'{type(currentbalance)}\' was inputted instead of \'<class \'int\'>\'')
+            return
+
+        return (currentbalance, currentcurrency, converted, convertto)
+
+# debug
+# instance1 = convertCurrency(gui=True)
+# print(instance1.getCurrency(currentcurrency="USD", currentbalance=20.00, newcurrency="gbp"))
