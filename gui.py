e0b87c422ade0b63fdcf9f2d1fc5c77595b0ba62
poiboi <33038633+poibear@users.noreply.github.com>
2022-08-06 18:06:32 -0700

    move _draw into __init__, use azure theme
    azure theme courtesy of rdbende (https://github.com/rdbende/Azure-ttk-theme)

--- gui.py
+++ gui.py
@@ -1,4 +1,9 @@
+import sys
+
+sys.path.insert(1, "C:\\Users\\JoshL\\Python\\projects\\currencyconvert") #allow import of currencyconvert from folder
+
 import tkinter as tk
+from tkinter import ttk
 from currencyconvert import convertCurrency
 
 class CurrencyConversion(tk.Frame):
@@ -6,20 +11,7 @@ class CurrencyConversion(tk.Frame):
     bgprocess = convertCurrency(gui=True)
 
     def __init__(self):
-        self.master = self._draw()
         self.bgprocess = convertCurrency(gui=True)
-        
-    def convInEntryBox(self, finalStringVar, currentCurrency, currentBalance, finalCurrency):
-        current = currentCurrency.get()
-        toconvert = finalCurrency.get()
-        balance = currentBalance.get()
-        info = self.bgprocess.getCurrency(current, balance, toconvert)
-        finalStringVar.set(info[2])
-        return info[2] #not necessary but recommended to info what we're changing
-
-    def _draw(self):
-        print('using default config...')
-        # base format
         mainframe = tk.Tk()
         mainframe.title('currency converter')
         mainframe.geometry('960x540')
@@ -34,44 +26,73 @@ class CurrencyConversion(tk.Frame):
             fromframe.grid_columnconfigure(rc, weight = 1)
             toframe.grid_columnconfigure(rc, weight = 1)
         
+        fromframe.grid_rowconfigure((0), weight = 1) #by default no weight
         toframe.grid_rowconfigure((0, 1), weight = 1)
         
         currencylist = self.bgprocess.currenciesToList()
+        
+        # design
+        mainfocusfont = ("Poppins", 25)
+        mainselectionfont = ("Poppins", 20)
+        dropdownlistfont = ("Poppins", 14)
 
         # elements
         currentbalancetext = tk.StringVar(fromframe) # ORIGINAL CURRENCY TEXT
-        currentbalance = tk.Entry(fromframe, textvariable=currentbalancetext)
+        currentbalancetext.set("Current Currency")
+        currentbalance = tk.Entry(fromframe, textvariable=currentbalancetext, font=mainfocusfont)
         
         tocurrencytext = tk.StringVar(toframe)
-        tocurrencybalance = tk.Entry(toframe, textvariable=tocurrencytext, state=tk.DISABLED)
+        tocurrencytext.set("Converted Currency")
+        tocurrencybalance = tk.Entry(toframe, textvariable=tocurrencytext, font=mainfocusfont, state=tk.DISABLED)
         
         currentcurrency = tk.StringVar(fromframe)
         currentcurrency.set(currencylist[currencylist.index('USD')]) #must add this function separate from var assignment to use .get function
-        currentcurrencydropdown = tk.OptionMenu(fromframe, currentcurrency, *currencylist)
+        # currentcurrencydropdown = tk.OptionMenu(fromframe, currentcurrency, *currencylist)
+        currentcurrencydropdown = ttk.Combobox(fromframe, width = 4, textvariable = currentcurrency, values=currencylist, state="readonly") #combobox for scrollbar
+        currentcurrencydropdown.config(font=mainselectionfont)
         
         finalcurrency = tk.StringVar(toframe)
         finalcurrency.set(currencylist[currencylist.index('USD')])
-        finalcurrencydropdown = tk.OptionMenu(toframe, finalcurrency, *currencylist)
+        # finalcurrencydropdown = tk.OptionMenu(toframe, finalcurrency, *currencylist)
+        finalcurrencydropdown = ttk.Combobox(toframe, width = 4, textvariable = finalcurrency, values = currencylist, state="readonly")
+        finalcurrencydropdown.config(font=mainselectionfont)
         
-        def conv():
+        fromframe.option_add("*TCombobox*Listbox.font", dropdownlistfont)
+        toframe.option_add("*TCombobox*Listbox.font", dropdownlistfont)
+
+        def convInEntryBox() -> int:
             current = currentcurrency.get()
-            toconvert = tocurrencytext.get()
+            toconvert = finalcurrency.get()
             balance = currentbalancetext.get()
-            info = self.bgprocess.getCurrency(current, balance, toconvert)
-            finalcurrency.set(info[2])
-            return info[2] #not necessary but recommended to info what we're changing
+            try:
+                info = self.bgprocess.getCurrency(current, balance, toconvert)
+                tocurrencytext.set(info[2])
+                return info[2] #not necessary but recommended to notify what we're changing, returns balance of final converted currency
+            except ValueError:
+                currentbalancetext.set("ENTER A VALID NUMBER")
+                convbtntext.set("convert")
         
-        convertbutton = tk.Button(toframe, text="convert", command=self.convInEntryBox(finalcurrency, ))
+        def clearInput(event=None): #event parameter required when binding, does nothing otherwise
+            currentbalancetext.set("")
+        
+        convbtntext = tk.StringVar(toframe)
+        convbtntext.set("convert")
+        convertbutton = tk.Button(toframe, textvariable=convbtntext, font=mainselectionfont, command=convInEntryBox)
         
         # init elements
         
         currentbalance.grid(row=0, column=0, sticky="nsew")
+        currentbalance.bind("<Button-1>", clearInput) #left click mouse button
+        
         currentcurrencydropdown.grid(row=0, column=1, sticky="nsew")
         
         tocurrencybalance.grid(row=0, column=0, sticky="nsew")
         finalcurrencydropdown.grid(row=0, column=1, sticky="nsew")
         
         convertbutton.grid(row=1, columnspan=2, sticky="nsew")
+        
+        mainframe.tk.call('source', 'azure.tcl') #import theme
+        mainframe.tk.call('set_theme', 'light')
         
         # init frames
         maincontent.pack(fill="both", expand=True)
@@ -80,7 +101,15 @@ class CurrencyConversion(tk.Frame):
         toframe.pack(side="bottom", fill="both", expand=True)
         
         print('done!')
-        return mainframe
+        self.master = mainframe
+        
+    def convInEntryBox(self, finalStringVar: tk.StringVar, currentCurrency: tk.StringVar, currentBalance: tk.StringVar, finalCurrency: tk.StringVar) -> int:
+        current = currentCurrency.get()
+        toconvert = finalCurrency.get()
+        balance = currentBalance.get()
+        info = self.bgprocess.getCurrency(current, balance, toconvert)
+        finalStringVar.set(info[2])
+        return info[2] #not necessary but recommended to info what we're changing        
     
     def run(self):
         def completeKill():
